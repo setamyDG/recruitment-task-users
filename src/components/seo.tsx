6ab312bb85b60ea19d063cbdb974c67ef6ce4755
setamyDG <setamy.dg@gmail.com>
2021-06-09 21:11:15 +0200

    fix: update seo

--- src/components/seo.tsx
+++ src/components/seo.tsx
@@ -8,28 +8,6 @@ type Props = {
 const SEO: FC<Props> = ({ title }) => (
   <Helmet>
     <title>{title}</title>
-    <meta name='title' content='React - Recruitment task' data-react-helmet='true' />
-    <meta property='og:title' content='DG - React recruitment task' react-helmet-data='true' />
-    <meta property='og:type' content='website' react-helmet-data='true' />
-    <meta property='twitter:card' content='summary' react-helmet-data='true' />
-    <meta property='twitter:creator' content='Daniel Gola' react-helmet-data='true' />
-    <meta property='twitter:title' content='DG - React recruitment task' react-helmet-data='true' />
-    <meta
-      property='twitter:description'
-      content='React application written in Typescript what allows to filter users taken from API'
-      react-helmet-data='true'
-    />
-    <meta
-      property='og:description'
-      content='React application written in Typescript what allows to filter users taken from API'
-      react-helmet-data='true'
-    />
-    <meta name='theme-color' content='#000000' react-helmet-data='true' />
-    <meta
-      name='description'
-      content='React application written in Typescript what allows to filter users taken from API'
-      react-helmet-data='true'
-    />
     <link href='https://fonts.googleapis.com/css2?family=Work+Sans:wght@300;500&display=swap' rel='stylesheet' />
     <link href='https://fonts.googleapis.com/css2?family=Inter:wght@700&display=swap' rel='stylesheet' />
   </Helmet>

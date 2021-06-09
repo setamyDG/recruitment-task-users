8985a046810ae74b51111a20f0bba9bdec20e91e
setamyDG <setamy.dg@gmail.com>
2021-06-09 15:58:34 +0200

    update

--- src/components/information/steps.ts
+++ src/components/information/steps.ts
@@ -17,7 +17,7 @@ export default [
   {
     id: 4,
     description:
-      'Dodaj pole do wyszukiwania użytkownika po imieniui nazwisku i  zaimplementujmechanizmwyszukiwania po stronie front end. Pamiętaj,żeby kod i testy spełniałystandardy produkcyjne, jeśli chodzi o jakość i dobrepraktyki',
+      'Dodaj pole do wyszukiwania użytkownika po imieniui nazwisku i  zaimplementuj mechanizm wyszukiwania po stronie front end. Pamiętaj,żeby kod i testy spełniałystandardy produkcyjne, jeśli chodzi o jakość i dobrepraktyki',
   },
   {
     id: 5,

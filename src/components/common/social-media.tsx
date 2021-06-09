f69b9e7f3f5469ed4c90d136b48efaa9f1078392
setamyDG <setamy.dg@gmail.com>
2021-06-09 21:21:05 +0200

    feat: delete linkedin icon and url

--- src/components/common/social-media.tsx
+++ src/components/common/social-media.tsx
@@ -1,9 +1,8 @@
 import React, { FC } from 'react';
 import { ReactComponent as GithubLogo } from '@assets/icons/github.svg';
-import { ReactComponent as LinkedinLogo } from '@assets/icons/linkedin.svg';
 import { ReactComponent as FacebookLogo } from '@assets/icons/facebook.svg';
 import { ReactComponent as FrontendLogo } from '@assets/icons/front-end.svg';
-import { GITHUB, LINKEDIN, CODETAIN, FACEBOOK } from '@constants/urls';
+import { GITHUB, CODETAIN, FACEBOOK } from '@constants/urls';
 import { LogoWrapper, Container } from './social-media.styled';
 
 const SocialMedia: FC = () => (
@@ -11,11 +10,6 @@ const SocialMedia: FC = () => (
     <a target='_blank' rel='noopener noreferrer' href={GITHUB}>
       <LogoWrapper>
         <GithubLogo />
-      </LogoWrapper>
-    </a>
-    <a target='_blank' rel='noopener noreferrer' href={LINKEDIN}>
-      <LogoWrapper>
-        <LinkedinLogo />
       </LogoWrapper>
     </a>
     <a target='_blank' rel='noopener noreferrer' href={FACEBOOK}>
